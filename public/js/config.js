7ffcb887c6480274642ba18ebda66488e41d3e00
haha <zhaoluyun@163.com>
2017-09-02 10:19:59 +0800

    讲师查看功能

--- public/js/config.js
+++ public/js/config.js
@@ -4,8 +4,14 @@ require.config({//配置别名
         jquery: 'jquery/jquery.min',
         cookie: 'jquery-cookie/jquery.cookie',
         template: 'artTemplate/template-web',
+        bootstrap : 'bootstrap/js/bootstrap.min',
         common: '../js/common',
         login: '../js/login',
         teacherlist : '../js/teacher-list'
+    },
+    shim : {//做兼容处理用
+        bootstrap : {
+            deps : ['jquery']//deps 依赖
+        }
     }
 });
--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,7 +1,8 @@
 /**
  * Created by luyun on 2017/9/1.
  */
-define(['jquery','template'],function($,template){
+//bootstrap的js插件需要依赖于jQuery，bootstrap的js文件不是标准的require.js模块,则需要做兼容处理
+define(['jquery','template','bootstrap'],function($,template){
     //console.log('12345');
     //调用后台接口获取列表数据
     $.ajax({
@@ -13,6 +14,33 @@ define(['jquery','template'],function($,template){
             //解析数据渲染页面
             var html = template('teacherTpl',{list : data.result});
             $('#teacherInfo').html(html);
+
+
+
+            $('.preview').click(function(){
+                //console.log(1234);
+                //获取当前ID
+                var td = $(this).closest('td');//closest()选中当前标签最近的父元素
+                var tcId = td.attr('data-tcId');
+                //根据ID掉接口
+                $.ajax({
+                    type : 'get',
+                    url : '/api/teacher/view',
+                    data : {tc_id : tcId},
+                    dataType : 'json',
+                    success : function(data){
+                        console.log(data);
+                        //解析数据渲染模板
+                        var html = template('modalTpl',data.result);
+                        $('#modalInfo').html(html);
+                        //显示弹框
+                        $('#teacherModal').modal();
+                    }
+                })
+            })
+
+
+
         }
     });
 });
